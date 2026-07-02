1d71b2cc7bc301040be48f51577e7df1da70a00f
HuoYan1213 <huoyan0928@gmail.com>
2026-07-02 11:33:25 +0800

    init commit

---
+++ src/main/java/com/tarumt/tarumt_resorts/TarumtResortsApplication.java
@@ -0,0 +1,13 @@
+package com.tarumt.tarumt_resorts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarumtResortsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TarumtResortsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tarumt/tarumt_resorts/TarumtResortsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarumt.tarumt_resorts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarumtResortsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
