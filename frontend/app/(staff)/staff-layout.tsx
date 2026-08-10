5a812b6c08b3bfe55ef35e549a28172f6a3c5386
HuoYan1213 <huoyan0928@gmail.com>
2026-08-10 13:36:28 +0800

    feat: implement MyList ADT and create staff dashboard layout component

--- frontend/app/(staff)/staff-layout.tsx
+++ frontend/app/(staff)/staff-layout.tsx
@@ -4,26 +4,25 @@ import TopBar from "../../components/TopBar";
 import { useLocation } from "react-router";
 import { useState } from "react";
 import { PATHS } from "../../lib/config/routes";
-import { motion } from "motion/react";
 
 export default function AdminLayout() {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const pathname = useLocation().pathname;
-    const path = PATHS.find((path) => path.to === pathname);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const pathname = useLocation().pathname;
+  const path = PATHS.find((path) => path.to === pathname);
 
-    return (
-        <main className="flex h-screen flex-1 overflow-hidden">
-            <SideNav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-            <section className="flex-1 flex flex-col w-full min-h-0 overflow-hidden">
-                <TopBar
-                    title={path?.label || "Unavailable"}
-                    desc={path?.desc || "Unavailable"}
-                    setMenuOpen={setMenuOpen}
-                />
-                <div className="flex-1 min-h-0 overflow-y-auto">
-                    <Outlet />
-                </div>
-            </section>
-        </main>
-    );
+  return (
+    <main className="flex h-screen flex-1 overflow-hidden">
+      <SideNav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <section className="flex-1 flex flex-col w-full min-h-0 overflow-hidden">
+        <TopBar
+          title={path?.label || "Unavailable"}
+          desc={path?.desc || "Unavailable"}
+          setMenuOpen={setMenuOpen}
+        />
+        <div className="flex-1 min-h-0 overflow-y-auto">
+          <Outlet />
+        </div>
+      </section>
+    </main>
+  );
 }
