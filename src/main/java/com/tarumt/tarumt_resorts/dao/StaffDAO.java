db15146591db28d981ade803fde3d3aa021833ee
HuoYan1213 <huoyan0928@gmail.com>
2026-08-13 23:08:12 +0800

    refactor: replace DTO from "/entity/dto" to "dto"

--- src/main/java/com/tarumt/tarumt_resorts/dao/StaffDAO.java
+++ src/main/java/com/tarumt/tarumt_resorts/dao/StaffDAO.java
@@ -2,8 +2,6 @@ package com.tarumt.tarumt_resorts.dao;
 
 import com.tarumt.tarumt_resorts.entity.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface StaffDAO extends JpaRepository<Staff, String> {
 }
