5a5b28e17402b4c6ad0dca33f9b91eda40b9c9ea
kentsee02 <kentsee02@gmail.com>
2026-08-14 21:22:02 +0800

    refactor: replace List with MyList in housekeeping control and boundary classes

--- src/main/java/com/tarumt/tarumt_resorts/dao/HousekeepingTaskDAO.java
+++ src/main/java/com/tarumt/tarumt_resorts/dao/HousekeepingTaskDAO.java
@@ -3,16 +3,14 @@ package com.tarumt.tarumt_resorts.dao;
 import com.tarumt.tarumt_resorts.entity.HousekeepingTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface HousekeepingTaskDAO extends JpaRepository<HousekeepingTask, Long> {
 
     // latest task first - used to determine a room's current housekeeping stage
-    List<HousekeepingTask> findByRoom_RoomIdOrderByCreatedAtDesc(String roomId);
+    Iterable<HousekeepingTask> findByRoom_RoomIdOrderByCreatedAtDesc(String roomId);
 
     // chronological order - used when walking through a room's full history
     // to detect cleaning cycles (DIRTY -> READY_FOR_CHECKIN) for reports
-    List<HousekeepingTask> findByRoom_RoomIdOrderByCreatedAtAsc(String roomId);
+    Iterable<HousekeepingTask> findByRoom_RoomIdOrderByCreatedAtAsc(String roomId);
 
-    List<HousekeepingTask> findAllByOrderByCreatedAtAsc();
+    Iterable<HousekeepingTask> findAllByOrderByCreatedAtAsc();
 }
