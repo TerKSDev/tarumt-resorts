f329c8749ac3bc27e3e0c1b9ebcb57f0dd466c04
HuoYan <huoyan0928@gmail.com>
2026-08-26 12:31:12 +0800

    feat: implement BookingDAO with custom query methods and remove seed script

--- src/main/java/com/tarumt/tarumt_resorts/dao/BookingDAO.java
+++ src/main/java/com/tarumt/tarumt_resorts/dao/BookingDAO.java
@@ -16,7 +16,6 @@ public interface BookingDAO extends JpaRepository<Booking, Long> {
     @org.springframework.data.jpa.repository.Query(value = "SELECT * FROM bookings WHERE date(check_in_date) = :today OR date(check_out_date) = :today", nativeQuery = true)
     Booking[] findTodayArrivalAndDeparture(@org.springframework.data.repository.query.Param("today") String today);
 
-
     // Custom Query Method (Java Persistence Query Language: JPQL)
     // Ter Kean Sen: Get bookings that are active or checked in
     @org.springframework.data.jpa.repository.Query("SELECT b FROM Booking b WHERE b.room.roomId = :roomId AND (b.status = :status1 OR b.status = :status2)")
